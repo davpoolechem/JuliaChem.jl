1498ce1df286b32651e01c86b6de3604f467549a
david <david@localhost.localdomain>
2019-03-31 16:05:41 -0500

    Move script-building functionality into input.jl rather than julichem.jl

--- input.jl
+++ input.jl
@@ -1,4 +1,23 @@
-#choose input file here
+#------------------------#
+# select input file here #
+#------------------------#
 input_file = "examples/sto3g-water.jl"
 
 Base.include(@__MODULE__,input_file)
+
+#-----------------------------#
+# build execution script here #
+#-----------------------------#
+function script()
+    #read in input file
+    @time flags::Flags, coord::Array{Float64,2} = do_input()
+
+    #analyze molecular coordinates
+    @time do_coordinate_analysis(coord)
+
+    #perform scf calculation
+    @time scf::Data = do_rhf(flags)
+
+    #determine wavefunction properties
+    #@time do_properties(scf,flags)
+end
--- julichem.jl
+++ julichem.jl
@@ -14,17 +14,8 @@ function julia_main()
     println(" ")
     println("                                 Authors: David Poole                          ")
     println(" ")
-    #read in input file
-    @time flags::Flags, coord::Array{Float64,2} = do_input()
 
-    #analyze molecular coordinates
-    @time do_coordinate_analysis(coord)
-
-    #perform scf calculation
-    @time scf::Data = do_rhf(flags)
-
-    #determine wavefunction properties
-    #@time do_properties(scf,flags)
+    script()
 
     #we have run to completion! :)
     println("--------------------------------------------------------------------------------------")
