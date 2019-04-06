72b0e94bd8cc1e90d69db319777a9e50660707a5
david <david@localhost.localdomain>
2019-04-05 22:34:56 -0500

    Small change to julichem run script, to add MPI rank count

--- julichem.sh
+++ julichem.sh
@@ -5,4 +5,4 @@ SYSIMG=""
 
 DO_MPIRUN='/opt/pgi/linux86-64/2018/mpi/openmpi/bin/mpirun'
 DO_JULIARUN='~/Local/Documents/Programs/julia-1.1.0/julia'
-JULIA_NUM_THREADS=${1} $DO_MPIRUN ~/Local/Documents/Programs/julia-1.1.0/julia $SYSIMG $MODULE_OPT $CODE_OPT src/core/julichem_shell.jl
+JULIA_NUM_THREADS=${2} $DO_MPIRUN -np ${1} ~/Local/Documents/Programs/julia-1.1.0/julia $SYSIMG $MODULE_OPT $CODE_OPT src/core/julichem_shell.jl
