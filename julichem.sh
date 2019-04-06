e93fc043918dfd49b19c61e1cd8f6524739817f0
david <david@localhost.localdomain>
2019-04-05 22:03:53 -0500

    Merge branch 'modules-dox'

--- julichem.sh
+++ julichem.sh
@@ -3,4 +3,4 @@ CODE_OPT="-O0 --check-bounds=yes --math-mode=fast --inline=yes"
 #SYSIMG="-J /home/david/.julia/packages/PackageCompiler/oT98U/sysimg/backup/native/sys.ji"
 SYSIMG=""
 
-JULIA_NUM_THREADS=${2} ~/Local/Documents/Programs/julia-1.1.0/julia $SYSIMG $MODULE_OPT $CODE_OPT src/core/julichem_shell.jl ${1}
+JULIA_NUM_THREADS=${1} ~/Local/Documents/Programs/julia-1.1.0/julia $SYSIMG $MODULE_OPT $CODE_OPT src/core/julichem_shell.jl 
