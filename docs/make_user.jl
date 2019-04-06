44fdb0680f289134aeee643e49813b1451781136
david <david@localhost.localdomain>
2019-04-05 19:01:47 -0500

    Add LaTeX support to documentation generation

--- docs/make_user.jl
+++ docs/make_user.jl
@@ -1,9 +1,12 @@
 using Documenter, Input, Molecule, RHF, Properties
+using DocumenterLaTeX
 
 makedocs(
+    authors="David Poole",
     sitename="JuliChem Documentation",
     modules = [Input,
                Molecule,
                RHF,
                Properties],
+    #format = LaTeX()
 )
