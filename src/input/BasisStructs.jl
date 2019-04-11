bc0f5b2546282e56c81048eecb389418e4eb881a
davpoolechem <davpoole@iastate.edu>
2019-04-11 11:50:47 -0500

    Add shell pair and shell quartet structs

--- src/input/BasisStructs.jl
+++ src/input/BasisStructs.jl
@@ -12,6 +12,23 @@ Shell(am) = Shell(am,am_to_nbas_cart(am),1)
     return am*(am+1)/2
 end
 
+mutable struct ShPair
+    sh_a::Shell
+    sh_b::Shell
+
+    am2::Int64
+    nbas2::Int64
+end
+export ShPair
+
+ShPair(sh_a,sh_b) = ShPair(sh_a,sh_b,sh_a.am2+sh_b.am2,sh_a.nbas2*sh_b.nbas2)
+
+mutable struct ShQuartet
+    bra::ShPair
+    ket::ShPair
+end
+export ShQuartet
+
 mutable struct Basis
     shells::Array{Shell,1}
 end
