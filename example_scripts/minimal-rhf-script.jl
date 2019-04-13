ec13d697de4e159a7f4c1b4bf2dee285dae1ebb0
david <david@localhost.localdomain>
2019-04-13 09:51:23 -0500

    Small changes to SnoopCompile support

--- example_scripts/minimal-rhf-script.jl
+++ example_scripts/minimal-rhf-script.jl
@@ -31,44 +31,44 @@ end
 #--------------------------------------------#
 # we want to precompile all involved modules #
 #--------------------------------------------#
-if (isfile("snoop/precompile_Base.jl"))
-    include("snoop/precompile_Base.jl")
+if (isfile("../snoop/precompile_Base.jl"))
+    include("../snoop/precompile_Base.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_Compat.jl"))
-    include("snoop/precompile_Compat.jl")
+if (isfile("../snoop/precompile_Compat.jl"))
+    include("../snoop/precompile_Compat.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_InputIntegrals.jl"))
-    include("snoop/precompile_InputIntegrals.jl")
+if (isfile("../snoop/precompile_InputIntegrals.jl"))
+    include("../snoop/precompile_InputIntegrals.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_JCInput.jl"))
-    include("snoop/precompile_JCInput.jl")
+if (isfile("../snoop/precompile_JCInput.jl"))
+    include("../snoop/precompile_JCInput.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_JCRHF.jl"))
-    include("snoop/precompile_JCRHF.jl")
+if (isfile("../snoop/precompile_JCRHF.jl"))
+    include("../snoop/precompile_JCRHF.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_LinearAlgebra.jl"))
-    include("snoop/precompile_LinearAlgebra.jl")
+if (isfile("../snoop/precompile_LinearAlgebra.jl"))
+    include("../snoop/precompile_LinearAlgebra.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_Main.jl"))
-    include("snoop/precompile_Main.jl")
+if (isfile("../snoop/precompile_Main.jl"))
+    include("../snoop/precompile_Main.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_MPI.jl"))
-    include("snoop/precompile_MPI.jl")
+if (isfile("../snoop/precompile_MPI.jl"))
+    include("../snoop/precompile_MPI.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_RHFSCF.jl"))
-    include("snoop/precompile_RHFSCF.jl")
+if (isfile("../snoop/precompile_RHFSCF.jl"))
+    include("../snoop/precompile_RHFSCF.jl")
     _precompile_()
 end
-if (isfile("snoop/precompile_unknown.jl"))
-    include("snoop/precompile_unknown.jl")
+if (isfile("../snoop/precompile_unknown.jl"))
+    include("../snoop/precompile_unknown.jl")
     _precompile_()
 end
 
