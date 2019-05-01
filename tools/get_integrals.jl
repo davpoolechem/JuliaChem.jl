1b18f46c60496c8564b877be0bc279ba06fc4435
davpoolechem <davpoole@iastate.edu>
2019-05-01 17:21:49 -0500

    Start work on importing one-electron integrals from GAMESS

--- tools/get_integrals.jl
+++ tools/get_integrals.jl
@@ -1,8 +1,10 @@
-function get_integrals(input::String, nshell::Int64)
+function get_tei_integrals(input::String, nbf::Int64)
+    #--read in input file--#
     f_int::IOStream = open(input)
         integral_strings::Array{String,1} = readlines(f_int)
     close(f_int)
 
+    #--process read-in file to create integral list--#
     integrals_temp::Array{Array{Real,1}} = []
 	for iline::String in integral_strings
       if (!occursin("II",iline) && !occursin("TOTAL NUMBER OF NONZERO TWO-ELECTRON INTEGRALS",iline))
@@ -24,6 +26,7 @@ function get_integrals(input::String, nshell::Int64)
       end
     end
 
+    #--format integrals for json format--#
     integrals::Array{String,1} = []
     push!(integrals,"{")
     push!(integrals,"   \"Input\":\"Two-Electron\",")
@@ -43,49 +46,75 @@ function get_integrals(input::String, nshell::Int64)
     end
     push!(integrals,"}")
 
-    f_int_w::IOStream = open("integrals.log","w")
+    #--write json block to output file--#
+    f_tei_w::IOStream = open("tei_integrals.log","w")
         for iline in integrals
-            write(f_int_w,"$iline\n")
+            write(f_tei_w,"$iline\n")
         end
-    close(f_int_w)
-
-#=
-	integrals_temp_b::Array{Array{Array{Array{Float64,1}}}} = []
-
-	for ish::Int64 in 1:nshell
-	  push!(integrals_temp_b,[])
-
-      for jsh::Int64 in 1:nshell
-	    push!(integrals_temp_b[ish],[])
-
-        for ksh::Int64 in 1:nshell
-	      push!(integrals_temp_b[ish][jsh],[])
-
-          for lsh::Int64 in 1:nshell
-	        push!(integrals_temp_b[ish][jsh][ksh], zero(Float64))
-		  end
-		end
-	  end
-    end
-
-	for int in 1:length(integrals_temp)
-	  ish = integrals_temp[int][1]
-	  jsh = integrals_temp[int][2]
-	  ksh = integrals_temp[int][3]
-	  lsh = integrals_temp[int][4]
-      integrals_temp_b[ish][jsh][ksh][lsh] = integrals_temp[int][5]
-    end
-
-	integrals::Array{String,1} = []
-    for ish::Int64 in 1:nshell, jsh::Int64 in 1:nshell
-      for ksh::Int64 in 1:nshell, lsh::Int64 in 1:nshell
-	    integral::Float64 = integrals_temp_b[ish][jsh][ksh][lsh]
-	    push!(integrals,"[$ish, $jsh, $ksh, $lsh, $integral]")
-	  end
-    end
-
-	#display(integrals)
-	=#
+    close(f_tei_w)
 end
 
-get_integrals("sto3g-water.int",7)
+function get_oei_integrals(input::String, nbf::Int64)
+    #--read in input file--#
+    f_int::IOStream = open(input)
+        integral_strings::Array{String,1} = readlines(f_int)
+    close(f_int)
+
+    #--determine starting locations of OEI blocks--#
+    ovr_start = 0
+    ovr_end = 0
+
+    hcore_start = 0
+    hcore_end = 0
+
+    kei_start = 0
+    kei_end = length(integral_strings)
+
+    for i in 1:length(integral_strings)
+        if (occursin("OVERLAP MATRIX",integral_strings[i]))
+            ovr_start = i
+        elseif (occursin("BARE NUCLEUS HAMILTONIAN INTEGRALS",integral_strings[i]))
+            hcore_start = i
+            ovr_end = i-1
+        elseif (occursin("KINETIC ENERGY INTEGRALS",integral_strings[i]))
+            kei_start = i
+            hcore_end = i-1
+        end
+    end
+
+    ovr = integral_strings[ovr_start:ovr_end]
+    hcore = integral_strings[hcore_start:hcore_end]
+    kei = integral_strings[kei_start:kei_end]
+
+    #display(ovr[3+(1-1)*(nbf+3)][17:25])
+
+    ovr::Array{Real,1} = []
+    iblock::Int64 = 0
+    while (3+iblock*(nbf+3) < length(ovr))
+        columns::String = ovr[3+iblock*(nbf+3)]
+        icols::Array{Int64,1} = [ parse(Int64,columns[17:min(25,length(columns))]) ]
+
+        if (length(columns) > 25)
+            push!(icols,parse(Int64,columns[28:min(36,length(columns))]))
+        end
+        if (length(columns)) > 37
+            push!(icols,parse(Int64,columns[39:min(47,length(columns))]))
+        end
+        if (length(columns)) > 48
+            push!(icols,parse(Int64,columns[50:min(58,length(columns))]))
+        end
+        if (length(columns)) > 59
+            push!(icols,parse(Int64,columns[61:min(69,length(columns))]))
+        end
+
+        first = 5+iblock*(nbf+3)
+        last = min(4+nbf+iblock*(nbf+3),length(ovr))
+        display(ovr[first:last])
+
+
+
+        iblock += 1
+    end
+end
+
+get_oei_integrals("sto3g-water.oei",7)
