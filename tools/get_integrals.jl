cc7d1b24302d0a096b5550d99650c043cedf4687
davpoolechem <davpoole@iastate.edu>
2019-05-29 16:11:28 -0500

    Update two-electron integrals and tei reading

--- tools/get_integrals.jl
+++ tools/get_integrals.jl
@@ -4,72 +4,40 @@ function get_tei_integrals(input::String, nbf::Int64)
         integral_strings::Array{String,1} = readlines(f_int)
     close(f_int)
 
-    #--process read-in file to create integral list--#
-    integrals_temp::Array{Array{Real,1}} = []
-	for iline::String in integral_strings
-      if (!occursin("II",iline) && !occursin("TOTAL NUMBER OF NONZERO TWO-ELECTRON INTEGRALS",iline))
-	    integral_line_1::Array{Real,1} = []
-		push!(integral_line_1,parse(Int64, iline[1:4]))
-		push!(integral_line_1,parse(Int64, iline[5:8]))
-		push!(integral_line_1,parse(Int64, iline[9:12]))
-		push!(integral_line_1,parse(Int64, iline[13:16]))
-		push!(integral_line_1,parse(Float64, iline[27:38]))
-	   	push!(integrals_temp,integral_line_1)
-
-        integral_line_2::Array{Real,1} = []
-	    push!(integral_line_2,parse(Int64, iline[39:43]))
-		push!(integral_line_2,parse(Int64, iline[44:47]))
-		push!(integral_line_2,parse(Int64, iline[48:51]))
-		push!(integral_line_2,parse(Int64, iline[52:55]))
-		push!(integral_line_2,parse(Float64, iline[66:77]))
-	   	push!(integrals_temp,integral_line_2)
-      end
-    end
-
-	sort!(integrals_temp)
-
-    #--format integrals for json format--#
     integrals::Array{String,1} = []
-	nbf2::Int64 = nbf*(nbf+1)/2
 
-	for bf in 1:nbf2
-		ibf::Int64 = ceil(((-1+sqrt(1+8*bf))/2))
-		jbf::Int64 = bf%ibf + 1
-		nadd::Int64 = 0
+    #--write tei block--#
+    push!(integrals,"{")
+    push!(integrals,"   \"Input\":\"Two-Electron\",")
+    for iline in integral_strings
+        display(iline)
+        i::Int64 = parse(Int64,iline[1:5])
+        j::Int64 = parse(Int64,iline[20:25])
+        k::Int64 = parse(Int64,iline[43:48])
+        l::Int64 = parse(Int64,iline[60:66])
 
-		push!(integrals,"{")
-		push!(integrals,"   \"Input\":\"Two-Electron-$bf\",")
+        integral::Float64 = parse(Float64,iline[67:end])
 
-	    for int::Int64 in 1:length(integrals_temp)
-	        ish::Int64 = integrals_temp[int][1]
-	        jsh::Int64 = integrals_temp[int][2]
-	        ksh::Int64 = integrals_temp[int][3]
-	        lsh::Int64 = integrals_temp[int][4]
-	        integral::Float64 = integrals_temp[int][5]
-			if (ish == ibf && jsh == jbf)
-				if (nadd == 0 && ish == ksh && jsh == lsh)
-					push!(integrals,"   \"tei\":[ [$ish, $jsh, $ksh, $lsh, $integral] ]")
-		        elseif (nadd == 0)
-		            push!(integrals,"   \"tei\":[ [$ish, $jsh, $ksh, $lsh, $integral],")
-		        elseif (ish == ksh && jsh == lsh)
-		            push!(integrals,"           [$ish, $jsh, $ksh, $lsh, $integral] ]")
-		        else
-		            push!(integrals,"           [$ish, $jsh, $ksh, $lsh, $integral],")
-		        end
-				nadd += 1
-			end
-	    end
-	    push!(integrals,"}")
-	end
+        if (i == 1 && j == 1 && k == 1 && l == 1 && length(integral_strings) == 1)
+            push!(integrals,"   \"tei\":[ [$i, $j, $k, $l, $integral] ]")
+        elseif (i == 1 && j == 1 && k == 1 && l == 1)
+            push!(integrals,"   \"tei\":[ [$i, $j, $k, $l, $integral],")
+        elseif (i == nbf && j == nbf && k == nbf && l == nbf)
+            push!(integrals,"           [$i, $j, $k, $l, $integral] ]")
+        else
+            push!(integrals,"           [$i, $j, $k, $l, $integral],")
+        end
+    end
+    push!(integrals,"}")
+    push!(integrals,"")
 
     #--write json block to output file--#
-    f_tei_w::IOStream = open("tei_integrals.log","w")
+    f_tei_w::IOStream = open("tools/tei_integrals.log","w")
         for iline in integrals
             write(f_tei_w,"$iline\n")
         end
     close(f_tei_w)
 end
-
 
 function get_oei_integrals(input::String, nbf::Int64)
     #--read in input file--#
@@ -149,11 +117,12 @@ function get_oei_integrals(input::String, nbf::Int64)
     push!(integrals,"")
 
     #--write json block to output file--#
-    f_tei_w::IOStream = open("oei_integrals.log","w")
+    f_oei_w::IOStream = open("tools/oei_integrals.log","w")
         for iline in integrals
-            write(f_tei_w,"$iline\n")
+            write(f_oei_w,"$iline\n")
         end
-    close(f_tei_w)
+    close(f_oei_w)
 end
 
+get_tei_integrals("tools/sto3g-water.tei",7)
 get_oei_integrals("tools/sto3g-water.oei",7)
