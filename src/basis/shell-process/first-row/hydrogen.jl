9ffeb5d1c4922e8291b80d9ab1ca768fb6d6fd19
davpoolechem <davpoole@iastate.edu>
2019-06-26 12:24:23 -0500

    Add support for 6-31G hydrogen in basis set

--- src/basis/shell-process/first-row/hydrogen.jl
+++ src/basis/shell-process/first-row/hydrogen.jl
@@ -8,11 +8,24 @@ function process_H_shell(basis_set::Basis, atom_idx::Int64,
 
   #== STO-3G basis ==#
   if (basis_set.model == "STO-3G")
-    #== first STO-3G H shell ==#
+    #== first STO-3G H s shell ==#
     shell_am::Int64 = 1
     shell::Shell = Shell(atom_idx, atom_center, shell_am)
     add_shell(basis_set,deepcopy(shell))
     basis_set.norb += 1
-  end
+
+  #== 6-31G basis ==#
+  elseif (basis_set.model == "6-31G")
+    #== first STO-3G H s shell ==#
+    shell_am_1::Int64 = 1
+    shell_1::Shell = Shell(atom_idx, atom_center, shell_am_1)
+    add_shell(basis_set,deepcopy(shell))
+    basis_set.norb += 1
+
+    #== second STO-3G H s shell ==#
+    shell_am_2::Int64 = 1
+    shell_2::Shell = Shell(atom_idx, atom_center, shell_am_2)
+    add_shell(basis_set,deepcopy(shell))
+    basis_set.norb += 1
 end
 export process_H_shell
