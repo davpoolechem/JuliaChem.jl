ad50916c8d35a4def19ca678f7b8e1a927177d7f
David Poole <davpoole@iastate.edu>
2019-09-13 09:37:37 -0500

    Move basis set record to new folder

--- src/runtime/JCRuntime.jl
+++ src/runtime/JCRuntime.jl
@@ -2,22 +2,31 @@ using MPI
 
 #== initialize JuliaChem runtime ==#
 function initialize()
+  
   #== initialize MPI ==#
   if(!MPI.Initialized())
     MPI.Init()
   else
     println("JuliaChem has already been initialized!")
   end
+
+  #==set up scratch directory==#
+  
 end
 export initialize
 
 #== finalize JuliaChem runtime ==#
 function finalize() 
+  
   #== finalize MPI ==#
   if(!MPI.Finalized())
     MPI.Finalize()
   else
     println("JuliaChem has already been finalized!")
   end
+  
+  #==clean scratch directory==#
+
+
 end
 export finalize
