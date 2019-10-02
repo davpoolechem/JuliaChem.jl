c474fdeeb2b402274c42e077f8f88667f534e2a4
David Poole <davpoole@iastate.edu>
2019-10-02 14:46:09 -0500

    Remove juliachem script

--- juliachem.sh
+++
@@ -1,7 +0,0 @@
-MODULE_OPT=" " 
-CODE_OPT="-O2 --check-bounds=yes --math-mode=ieee"
-#SYSIMG="-J /home/david/.julia/packages/PackageCompiler/oT98U/sysimg/backup/native/sys.ji"
-SYSIMG=""
-
-echo "Performing calculation..."
-JULIA_NUM_THREADS=${4} mpirun -np ${3} julia $MODULE_OPT $CODE_OPT ${1} ${2}
