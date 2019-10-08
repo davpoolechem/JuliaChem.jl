b6c6940a5d4678a7879c9b2d256bab57298154ec
David Poole <davpoole@iastate.edu>
2019-10-08 09:09:43 -0500

    Switch from static to shared simint library

--- src/eri/simint.jl
+++ src/eri/simint.jl
@@ -6,28 +6,28 @@ using StaticArrays
 const DIRECTORY = "/export/home/david/projects/Julia/JuliaChem.jl"
 
 @inline function initialize()
-  ccall((:initialize_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid, ())
+  ccall((:initialize_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid, ())
 end
 export initialize
 
 @inline function finalize()
-  ccall((:finalize_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid, ())
+  ccall((:finalize_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid, ())
 end
 export finalize
 
 @inline function reset()
-  ccall((:reset_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid, ())
+  ccall((:reset_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid, ())
 end
 export reset
 
 @inline function get_julia_shell_info(shell::JCModules.BasisStructs.Shell)
-  ccall( (:get_julia_shell_info_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid,
+  ccall( (:get_julia_shell_info_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid,
     (Ref{JCModules.BasisStructs.Shell},), Ref(shell) )
 end
 export get_julia_shell_info
 
 @inline function get_simint_shell_info(shell::Int64)
-  ccall( (:get_simint_shell_info_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid,
+  ccall( (:get_simint_shell_info_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid,
     (Int64,), shell )
 end
 export get_simint_shell_info
@@ -40,7 +40,7 @@ export get_simint_shell_info
     nshell_simint += shell.nbas == 4 ? 2 : 1
   end
 
-  ccall( (:allocate_shell_array_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid,
+  ccall( (:allocate_shell_array_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid,
     (Int64,Int64), nshell, nshell_simint )
 
   return nshell_simint
@@ -48,13 +48,13 @@ end
 export allocate_shell_array
 
 @inline function add_shell(shell::JCModules.BasisStructs.Shell)
-  ccall( (:add_shell_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid,
+  ccall( (:add_shell_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid,
     (Ref{JCModules.BasisStructs.Shell},), Ref(shell) )
 end
 export add_shell
 
 @inline function normalize_shells()
-  ccall( (:normalize_shells_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid,
+  ccall( (:normalize_shells_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid,
     () )
 end
 export normalize_shells
@@ -70,31 +70,31 @@ export unnormalize_shell
 
 #= compute shell pairs =#
 @inline function create_ij_shell_pair(ish::Int64, jsh::Int64)
-  ccall( (:create_ij_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"),
+  ccall( (:create_ij_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"),
     Cvoid, (Int64, Int64), ish, jsh)
 end
 export create_ij_shell_pair
 
 @inline function allocate_kl_shell_pair(ksh::Int64, lsh::Int64)
-  ccall( (:allocate_kl_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"),
+  ccall( (:allocate_kl_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"),
     Cvoid, (Int64, Int64), ksh, lsh)
 end
 export allocate_kl_shell_pair
 
 @inline function create_kl_shell_pair(ksh::Int64, lsh::Int64)
-  ccall( (:create_kl_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"),
+  ccall( (:create_kl_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"),
     Cvoid, (Int64, Int64), ksh, lsh)
 end
 export create_kl_shell_pair
 
 @inline function fill_kl_shell_pair(ksh::Int64, lsh::Int64)
-  ccall( (:fill_kl_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"),
+  ccall( (:fill_kl_shell_pair_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"),
     Cvoid, (Int64, Int64), ksh, lsh)
 end
 export fill_kl_shell_pair
 
 @inline function compute_eris(eri::Vector{Float64})
-  ccall( (:compute_eris_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid,
+  ccall( (:compute_eris_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid,
     (Ptr{Float64},), eri)
 end
 export compute_eris
@@ -102,7 +102,7 @@ export compute_eris
 function retrieve_eris(ish::Int64, jsh::Int64, ksh::Int64, lsh::Int64,
   eri::Vector{T}) where {T<:AbstractFloat}
 
-  ccall( (:retrieve_eris_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libsimint.so"), Cvoid,
+  ccall( (:retrieve_eris_c, "/export/home/david/projects/Julia/JuliaChem.jl/src/eri/libjeri.so"), Cvoid,
     (Int64, Int64, Int64, Int64, Ptr{T}),
     ish, jsh, ksh, lsh, eri)
 end
