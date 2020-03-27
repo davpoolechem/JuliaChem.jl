6e3b7a640139952de264d99dfd7df94eac71bfee
David Poole <davpoole@iastate.edu>
2020-03-27 16:02:55 -0500

    SMall change to integral analysis script

--- tools/analyze_integrals.jl
+++ tools/analyze_integrals.jl
@@ -70,7 +70,8 @@ for key in keys(julia_dict)
   end 
   
   if has_julia_key
-    same = isapprox(gamess_dict[key], julia_dict[key], rtol = 1e-9)
+    same = gamess_dict[key] < 1.0E-10 && julia_dict[key] < 1.0E-10 ? true : 
+      isapprox(gamess_dict[key], julia_dict[key], rtol = 1e-9)
     if !same
       println("Not same!: ", key, "; ", gamess_dict[key], ", ", julia_dict[key])
     end
