f8af721bb1e1c8cd419b300a4a96d9d56de199db
David Poole <davpoole@iastate.edu>
2020-04-07 18:28:46 -0500

    Update python scripts

--- example_scripts/minimal-rhf-repl.py
+++ example_scripts/minimal-rhf-repl.py
@@ -10,16 +10,24 @@ from julia import JuliaChem
 #== JuliaChem execution script ==#
 #================================#
 def minimal_rhf(input_file):
-  #== read in input file ==#
-  molecule, driver, model, keywords = JuliaChem.JCInput.run(input_file, 
-    output="none")
+  try:  
+    #== read in input file ==#
+    molecule, driver, model, keywords = JuliaChem.JCInput.run(input_file, 
+      output="none")
 
-  #== generate basis set ==#
-  mol, basis = JuliaChem.JCBasis.run(molecule, model, output="none")
+    #== generate basis set ==#
+    mol, basis = JuliaChem.JCBasis.run(molecule, model, output="none")
 
-  #== perform scf calculation ==#
-  scf = JuliaChem.JCRHF.run(mol, basis, keywords, output="minimal")
+    #== perform scf calculation ==#
+    scf = JuliaChem.JCRHF.run(mol, basis, keywords, output="minimal")
 
-  #== reset JuliaChem runtime ==#
-  JuliaChem.reset()
-  return scf
+    #== reset JuliaChem runtime ==#
+    JuliaChem.reset()
+    return scf
+  except Exception as e:                                                        
+    bt = Base.catch_backtrace()                                                 
+    msg = Base.sprint(showerror, e, bt)                                         
+    print(msg)                                                                  
+                                                                                
+    JuliaChem.finalize()                                                        
+    exit()    
--- example_scripts/minimal-rhf.py
+++ example_scripts/minimal-rhf.py
@@ -5,24 +5,33 @@ import sys
 
 import julia
 from julia import JuliaChem
+from julia import Base 
 
 #================================#
 #== JuliaChem execution script ==#
 #================================#
 def minimal_rhf(input_file):
-  #== read in input file ==#
-  molecule, driver, model, keywords = JuliaChem.JCInput.run(input_file,
-    output="none")
+  try:
+    #== read in input file ==#
+    molecule, driver, model, keywords = JuliaChem.JCInput.run(input_file,
+      output="none")
 
-  #== generate basis set ==#
-  mol, basis = JuliaChem.JCBasis.run(molecule, model, output="none")
+    #== generate basis set ==#
+    mol, basis = JuliaChem.JCBasis.run(molecule, model, output="none")
 
-  #== perform scf calculation ==#
-  scf = JuliaChem.JCRHF.run(mol, basis, keywords["scf"], output="minimal")
+    #== perform scf calculation ==#
+    scf = JuliaChem.JCRHF.run(mol, basis, keywords["scf"], output="minimal")
   
-  #== reset JuliaChem runtime ==#
-  JuliaChem.reset()
-  return scf
+    #== reset JuliaChem runtime ==#
+    JuliaChem.reset()
+    return scf
+  except Exception as e: 
+    bt = Base.catch_backtrace()
+    msg = Base.sprint(showerror, e, bt)
+    print(msg)
+
+    JuliaChem.finalize()
+    exit()
 
 JuliaChem.initialize()
 scf = minimal_rhf(sys.argv[1])
