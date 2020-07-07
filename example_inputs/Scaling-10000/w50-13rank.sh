69ff00833f87759a08ad670234a86540743cbfe5
David Poole <davpoolechem@jlselogin2.ftm.alcf.anl.gov>
2020-07-07 20:47:40 +0000

    Rearrange benchmarks in example_inputs

--- example_inputs/Scaling-10000/w50-13rank.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-export OMP_NUM_THREADS=1
-export JULIA_NUM_THREADS=1
-#
-cd /home/davpoolechem/shared/Sandbox/Julia/JuliaChem.jl
-mpirun -np 13 julia --check-bounds=no --math-mode=fast --optimize=3 --inline=yes --compiled-modules=yes example_scripts/minimal-rhf-benchmark.jl example_inputs/Scaling-10000/w50-13rank.json
--- example_inputs/Scaling-Memory/w50-64rank.json.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-export OMP_NUM_THREADS=1
-export JULIA_NUM_THREADS=1
-#
-cd /home/davpoolechem/shared/Sandbox/Julia/JuliaChem.jl
-mpirun -np 64 julia --check-bounds=no --math-mode=fast --optimize=3 --inline=yes --compiled-modules=yes example_scripts/minimal-rhf-benchmark.jl example_inputs/Scaling-Memory/w50-64rank.json.json
