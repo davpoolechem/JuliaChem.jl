a2721653ee5c76e9a182bfd069666bc9db5a663b
David Poole <davpoolechem@jlselogin2.ftm.alcf.anl.gov>
2020-08-18 22:36:26 +0000

    Confirm correctness of f functions

--- tools/runtest-pbs.sh
+++ tools/runtest-pbs.sh
@@ -10,6 +10,6 @@
 #PBS -l jobfs=1Gb
 #PBS -l mem=128Gb
 #
-export JULIA_NUM_THREADS=8
+export JULIA_NUM_THREADS=16
 #
 julia --check-bounds=no --math-mode=fast --optimize=3 --inline=yes --compiled-modules=yes -E 'using Pkg; using JuliaChem; Pkg.test("JuliaChem")'
