40666469b250e6fd6ce369067699a0a9f745bdcf
David Poole <davpoole@iastate.edu>
2020-08-19 11:26:44 -0500

    Add minimal-rhf-travis file

--- test/runtests-travis.jl
+++ test/runtests-travis.jl
@@ -1,7 +1,7 @@
 import Pkg; Pkg.add("Test")
 import Test
 
-include("../example_scripts/minimal-rhf-travis.jl")
+include("../tools/minimal-rhf-travis.jl")
 
 #== select input files ==#
 directory = joinpath(@__DIR__, "../example_inputs/S22/")
@@ -13,13 +13,13 @@ JuliaChem.initialize()
 
 #== run S22 tests ==#
 Test.@testset "S22 accuracy test" begin
-  scf = minimal_rhf(inputs[3])
+  scf = travis_rhf(inputs[3])
   Test.@test scf["Energy"] ≈ -377.5889420312
 
-  scf = minimal_rhf(inputs[10])
+  scf = travis_rhf(inputs[10])
   Test.@test scf["Energy"] ≈ -270.9302743515 
 
-  scf = minimal_rhf(inputs[18])
+  scf = travis_rhf(inputs[18])
   Test.@test scf["Energy"] ≈ -286.9286565278 
 end
 
