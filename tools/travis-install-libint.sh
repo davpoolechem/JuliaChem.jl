e064686cfececbc3fe9d2603a261cd5676b60b86
David Poole <davpoole@iastate.edu>
2020-09-15 12:55:21 -0500

    Add patches to LibInt makefile during travis ci runs

--- tools/travis-install-libint.sh
+++ tools/travis-install-libint.sh
@@ -4,8 +4,41 @@
 cd /home/travis 
 git clone --quiet https://github.com/evaleev/libint.git
 
-#== install libint ==#
+#== patch libint Makefiles to remove CMake references ==#
 cd libint
+echo "33c33" &> Makefile.patch
+echo "< install:: all install_pkgconfig install_cmake install_inc install_data" >> Makefile.patch
+echo "---" >> Makefile.patch
+echo "> install:: all install_pkgconfig install_inc install_data" >> Makefile.patch
+echo "43,48d42" >> Makefile.patch
+echo "< endif" >> Makefile.patch
+echo "< " >> Makefile.patch
+echo "< ifdef cmakedir" >> Makefile.patch
+echo "< install_cmake::" >> Makefile.patch
+echo "<   \$(INSTALL) \$(INSTALLDIROPT) \$(DESTDIR)\$(cmakedir)" >> Makefile.patch
+echo "<   \$(INSTALL) \$(INSTALLLIBOPT) \$(SRCTOPDIR)/FindLibint2.cmake \$(DESTDIR)\$(cmakedir)" >> Makefile.patch
+patch Makefile Makefile.patch
+
+cd src/lib
+echo "2c2" &> MakeRules.in.patch
+echo "< .PHONY: default export install install_inc install_pkgconfig install_cmake install_target clean oclean distclean targetclean realclean" >> MakeRules.in.patch
+echo "---" >> MakeRules.in.patch
+echo "> .PHONY: default export install install_inc install_pkgconfig install_target clean oclean distclean targetclean realclean" >> MakeRules.in.patch
+echo "21c21" >> MakeRules.in.patch
+echo "< install:: install_inc install_target install_pkgconfig install_cmake install_data" >> MakeRules.in.patch
+echo "---" >> MakeRules.in.patch
+echo "> install:: install_inc install_target install_pkgconfig install_data" >> MakeRules.in.patch
+echo "51,56d50" >> MakeRules.in.patch
+echo "< endif" >> MakeRules.in.patch
+echo "< " >> MakeRules.in.patch
+echo "< ifdef cmakedir" >> MakeRules.in.patch
+echo "< install_cmake::" >> MakeRules.in.patch
+echo "<       \$(INSTALL) \$(INSTALLDIROPT) \$(DESTDIR)\$(cmakedir)" >> MakeRules.in.patch
+echo "<       \$(INSTALL) \$(INSTALLLIBOPT) \$(SRCTOPDIR)/FindLibint2.cmake \$(DESTDIR)\$(cmakedir)" >> MakeRules.in.patch
+patch MakeRules.in MakeRules.in.patch
+
+#== install libint ==#
+cd /home/travis/libint
 ./configure \
   --enable-shared=yes --prefix=/home/travis/libint-install \
   --enable-1body=0 --enable-eri=0 --with-max-am=3 \
@@ -13,7 +46,4 @@ cd libint
   --enable-eri2=no --enable-g12=no
 
 make -j2 -s
-set -x
-sed -i "s/install\_cmake //g" Makefile
-set +x
-make -j2 install
+make -j2 -s install
