8e98df76e287740da9f77e72fb7da386a4cd076d
David Poole <davpoole@iastate.edu>
2020-09-14 23:30:45 -0500

    Refine flags for libint install

--- tools/travis-install-libint.sh
+++ tools/travis-install-libint.sh
@@ -8,7 +8,10 @@ git clone --quiet https://github.com/evaleev/libint.git
 cd libint
 ./configure \
   --enable-shared=yes --prefix=/home/travis/libint-install \
-  --enable-1body=0 --enable-eri=0 --with-max-am=3
+  --enable-1body=0 --enable-eri=0 --with-max-am=3 \
+  --with-multipole-max-order=3 --enable-eri3=no \
+  --enable-eri2=no --enable-g12=no
+
 make -j2 -s
 sed -i "s/install_cmake //g" Makefile
 make -j2 -s install
