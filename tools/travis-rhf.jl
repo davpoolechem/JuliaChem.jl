c5c043de970406c2fbabfd4783c3e1676581c5a9
David Poole <davpoole@iastate.edu>
2020-10-20 12:32:18 -0500

    Another fix to travsi-rhf

--- tools/travis-rhf.jl
+++ tools/travis-rhf.jl
@@ -16,13 +16,13 @@ function travis_rhf(input_file)
     mol, basis = JuliaChem.JCBasis.run(molecule, model; output="verbose")          
 
     #== perform scf calculation ==#
-    rhfenergy = JuliaChem.JCRHF.Energy.run(mol, basis, keywords["scf"]; output="verbose") 
+    rhf_energy = JuliaChem.JCRHF.Energy.run(mol, basis, keywords["scf"]; output="verbose") 
  
     #== compute molecular properties ==# 
     JuliaChem.JCProperties.run(mol, basis, rhf_energy, keywords,
       output="verbose")  
 
-    return rhfenergy 
+    return rhf_energy 
   catch e                                                                       
     bt = catch_backtrace()                                                      
     msg = sprint(showerror, e, bt)                                              
