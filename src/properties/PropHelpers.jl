e6dda87dbf5a067ad2c7025c74385009cb63f14d
David Poole <davpoole@iastate.edu>
2020-10-28 09:07:33 -0500

    Further trim down dipole integral computation

--- src/properties/PropHelpers.jl
+++ src/properties/PropHelpers.jl
@@ -105,19 +105,13 @@ function compute_dipole(mol::Molecule,
         iorb = apos + ibas
         jorb = bpos + jbas
 
-        elec_dipole_matrix[icoord][iorb,jorb] += elec_dipole_block_JERI[abas*bbas*(icoord-1) + idx]
+        #elec_dipole_matrix[icoord][iorb,jorb] += elec_dipole_block_JERI[abas*bbas*(icoord-1) + idx]
+        elec_dipole[icoord] += P[iorb,jorb] * elec_dipole_block_JERI[abas*bbas*(icoord-1) + idx]
         idx += 1
       end
     end
   end
   
-  #== contract with energy-weighted density ==#
-  for icoord in 1:ncoord
-    for ibas in 1:basis.norb, jbas in 1:basis.norb
-      elec_dipole[icoord] += P[ibas,jbas] * elec_dipole_matrix[icoord][ibas, jbas]  
-    end
-  end
- 
   @printf("          %.8f   %.8f    %.8f         \n",
     nuc_dipole[1], nuc_dipole[2], nuc_dipole[3])
   @printf("          %.8f   %.8f    %.8f         \n",
