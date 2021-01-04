ab32adc133f8322732e39e285b916f41bc1f7fbd
David Poole <davpoolechem@jlselogin2.ftm.alcf.anl.gov>
2021-01-04 14:43:42 +0000

    Initial round of benchmarks

---
+++ example_inputs/ARPA-E/6-311/fig1e_1thread.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+source /etc/profile.d/modules.sh
+source ~/.bashrc
+#
+export JULIA_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+#
+mpirun -np 1 julia --check-bounds=no --math-mode=fast --optimize=3 --inline=yes --compiled-modules=yes /home/davpoolechem/shared/projects/Julia/JuliaChem.jl/example_scripts/minimal-rhf-benchmark.jl example_inputs/ARPA-E/6-311/fig1e.json
