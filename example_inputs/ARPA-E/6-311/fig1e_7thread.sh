8cb3da37fc64393789dba9bb03960307016562c0
David Poole <davpoolechem@skylake06.ftm.alcf.anl.gov>
2021-01-07 15:22:37 +0000

    Refine submission scripts

--- example_inputs/ARPA-E/6-311/fig1e_7thread.sh
+++ example_inputs/ARPA-E/6-311/fig1e_7thread.sh
@@ -6,4 +6,6 @@ source ~/.bashrc
 export JULIA_NUM_THREADS=7
 export OPENBLAS_NUM_THREADS=1
 #
-mpirun -np 1 julia --check-bounds=no --math-mode=fast --optimize=3 --inline=yes --compiled-modules=yes /home/davpoolechem/shared/projects/Julia/JuliaChem.jl/example_scripts/minimal-rhf-benchmark.jl example_inputs/ARPA-E/6-311/fig1e.json
+export RANKS_PER_NODE=1
+#
+${MPI_HOME}/bin/orterun --mca btl vader,self,tcp -np ${RANKS_PER_NODE} -map-by ppr:${RANKS_PER_NODE}:node --bind-to none --report-bindings julia --check-bounds=no --math-mode=fast --optimize=3 --inline=yes --compiled-modules=yes /home/davpoolechem/shared/projects/Julia/JuliaChem.jl/example_scripts/minimal-rhf-benchmark.jl example_inputs/ARPA-E/6-311/fig1e.json
