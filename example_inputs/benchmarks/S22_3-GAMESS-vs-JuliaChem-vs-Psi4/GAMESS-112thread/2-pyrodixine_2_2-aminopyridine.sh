5283d02e6d60992155dc37137a66bbd9cc8cd8cc
David Poole <davpoolechem@jlselogin2.ftm.alcf.anl.gov>
2021-01-24 06:36:47 +0000

    Redo 56-thread GAMESS benchmarks

--- example_inputs/benchmarks/S22_3-GAMESS-vs-JuliaChem-vs-Psi4/GAMESS-112thread/2-pyrodixine_2_2-aminopyridine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-source /etc/profile.d/modules.sh
-source ~/.bashrc
-#
-export OMP_NUM_THREADS=112
-#
-cd /home/davpoolechem/shared/gms-dp/gamess
-./rungms-dev S22_3/6-311++G_2d_2p/2-pyrodixine_2_2-aminopyridine.inp 00 1 1
--- example_inputs/benchmarks/S22_3-GAMESS-vs-JuliaChem-vs-Psi4/GAMESS-112thread/indole_benzene_2_t_shaped.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-source /etc/profile.d/modules.sh
-source ~/.bashrc
-#
-export OMP_NUM_THREADS=112
-#
-cd /home/davpoolechem/shared/gms-dp/gamess
-./rungms-dev S22_3/6-311++G_2d_2p/indole_benzene_2_t_shaped.inp 00 1 1
