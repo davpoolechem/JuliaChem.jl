62f8d490d433af92eec5b3b48d816ac9432b3600
David Poole <davpoolechem@jlselogin2.ftm.alcf.anl.gov>
2021-01-24 00:44:49 +0000

    New benchmarks for S22(3) test set comparing GAMESS, JuliaCHem, and Psi4

---
+++ example_inputs/benchmarks/S22_3-GAMESS-vs-JuliaChem-vs-Psi4/JuliaChem/phenol_trimer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+source /etc/profile.d/modules.sh
+source ~/.bashrc
+#
+export JULIA_NUM_THREADS=112
+export OPENBLAS_NUM_THREADS=1
+#
+/home/davpoolechem/programs/install/openmpi/bin/orterun --mca btl vader,self,tcp -np 1 -map-by ppr:1:node --bind-to none --report-bindings julia --check-bounds=no --math-mode=fast --optimize=3 --inline=yes --compiled-modules=yes /home/davpoolechem/shared/projects/Julia/JuliaChem.jl/example_scripts/minimal-rhf-benchmark.jl example_inputs/S22_3/6-311++G_2d_2p/phenol_trimer.json
