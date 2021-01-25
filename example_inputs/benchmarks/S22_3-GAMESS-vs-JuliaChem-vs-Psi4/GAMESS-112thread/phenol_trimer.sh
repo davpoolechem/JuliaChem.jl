a949b286f8d2c24511049fa68facb3297693b12b
David Poole <davpoolechem@jlselogin2.ftm.alcf.anl.gov>
2021-01-25 23:02:21 +0000

    Add 112-thread GAMESS benchmarks

---
+++ example_inputs/benchmarks/S22_3-GAMESS-vs-JuliaChem-vs-Psi4/GAMESS-112thread/phenol_trimer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+source /etc/profile.d/modules.sh
+source ~/.bashrc
+#
+export OMP_NUM_THREADS=112
+#
+cd /home/davpoolechem/shared/gms-dp/gamess
+./rungms-dev S22_3/6-311++G_2d_2p/phenol_trimer.inp 00 1 1
