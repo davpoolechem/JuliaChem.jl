53b50b80b4913b63a6f37474ca75c33d424a238b
David Poole <davpoolechem@jlselogin2.ftm.alcf.anl.gov>
2021-01-29 19:22:40 +0000

    Add 111-thread benchmarks for JuliaCHem and GAMESS

---
+++ example_inputs/benchmarks/S22_3-GAMESS-vs-JuliaChem-vs-Psi4/GAMESS-111thread/benzene_sandwich_trimer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+source /etc/profile.d/modules.sh
+source ~/.bashrc
+#
+export OMP_NUM_THREADS=111
+#
+cd /home/davpoolechem/shared/gms-dp/gamess
+./rungms-dev S22_3/6-311++G_2d_2p/benzene_sandwich_trimer.inp 00 1 1
