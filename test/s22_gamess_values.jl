8534ae32bd7ea06944694ec614e768adefe37b12
David Poole <davpoole@iastate.edu>
2021-02-15 14:28:49 -0600

    Add file with correct S22 GAMESS values

---
+++ test/s22_gamess_values.jl
@@ -0,0 +1,112 @@
+const S22_GAMESS = Dict(
+  1 => Dict(
+         "Energy" => -112.4047194144,
+         "Dipole" => 1.0E-6, #approximately 0.0        
+         "HOMO-LUMO Gap" => 0.6192
+       ),
+  2 => Dict(
+         "Energy" => -152.0671593526,
+         "Dipole" => 2.696653,
+         "HOMO-LUMO Gap" => 0.6493
+       ),
+  3 => Dict(
+         "Energy" => -377.5889420312,
+         "Dipole" => 1.0E-6, #approximately 0.0        
+         "HOMO-LUMO Gap" => 0.6283
+       ),
+  4 => Dict(
+         "Energy" => -337.9245601137,
+         "Dipole" => 1.0E-6, #approximately 0.0        
+         "HOMO-LUMO Gap" => 0.5964
+       ),
+  5 => Dict(
+         "Energy" => -825.0480089998,
+         "Dipole" => 1.0E-6, #approximately 0.0        
+         "HOMO-LUMO Gap" => 0.4622
+       ),
+  6 => Dict(
+         "Energy" => -623.3855095909,
+         "Dipole" => 3.239972, 
+         "HOMO-LUMO Gap" => 0.4068
+       ),
+  7 => Dict(
+         "Energy" => -916.1413738989,
+         "Dipole" => 1.990013, 
+         "HOMO-LUMO Gap" => 0.4054
+       ),
+  8 => Dict(
+         "Energy" => -80.4073036256,
+         "Dipole" => 1.0E-6, #approximately 0.0        
+         "HOMO-LUMO Gap" => 0.7288
+       ),
+  9 => Dict(
+         "Energy" => -156.0878533332,
+         "Dipole" => 1.0E-6, #approximately 0.0        
+         "HOMO-LUMO Gap" => 0.5424
+       ),  
+  10 => Dict(
+          "Energy" => -270.9302743515,
+          "Dipole" => 0.280546, 
+          "HOMO-LUMO Gap" => 0.4746  
+        ),
+  11 => Dict(
+          "Energy" => -461.4497600413,
+          "Dipole" => 1.0E-6, #approximately 0.0        
+          "HOMO-LUMO Gap" => 0.4538
+        ),
+  12 => Dict(
+          "Energy" => -525.4098675881,
+          "Dipole" => 0.227480, 
+          "HOMO-LUMO Gap" => 0.4372
+        ),
+  13 => Dict(
+          "Energy" => -825.0271971388,
+          "Dipole" => 5.604885, 
+          "HOMO-LUMO Gap" => 0.4447
+        ),
+  14 => Dict(
+          "Energy" => -592.2263255700,
+          "Dipole" => 1.860770, 
+          "HOMO-LUMO Gap" => 0.4031
+        ),
+  15 => Dict(
+          "Energy" => -916.1246231957,
+          "Dipole" => 3.478761, 
+          "HOMO-LUMO Gap" => 0.4154
+        ),
+  16 => Dict(
+          "Energy" => -154.8750579586,
+          "Dipole" => 0.493345, 
+          "HOMO-LUMO Gap" => 0.5441
+        ),
+  17 => Dict(
+          "Energy" => -306.7601557517,
+          "Dipole" => 2.422111, 
+          "HOMO-LUMO Gap" => 0.4736
+        ),
+  18 => Dict(
+          "Energy" => -286.9286565278,
+          "Dipole" => 1.770430, 
+          "HOMO-LUMO Gap" => 0.4742
+        ),
+  19 => Dict(
+          "Energy" => -323.6144109921,
+          "Dipole" => 4.142949, 
+          "HOMO-LUMO Gap" => 0.4757
+        ),
+  20 => Dict(
+          "Energy" => -461.4537798340,
+          "Dipole" => 0.651396, 
+          "HOMO-LUMO Gap" => 0.4563
+        ),
+  21 => Dict(
+          "Energy" => -592.2351527581,
+          "Dipole" => 3.194881, 
+          "HOMO-LUMO Gap" => 0.3869
+        ),
+  22 => Dict(
+          "Energy" => -611.1928406081,
+          "Dipole" => 3.486793,
+          "HOMO-LUMO Gap" => 0.4226
+        )
+)
